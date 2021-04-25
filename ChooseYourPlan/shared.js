69f12b36ee92951cee54d938f0d3d375b754958a
justinmorales1 <jm.em0210@gmail.com>
2021-04-25 12:24:36 -0500

    Cleaning up code

--- ChooseYourPlan/shared.js
+++ ChooseYourPlan/shared.js
@@ -7,21 +7,29 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function() {
-        modal.style.display = "block";
-        backdrop.style.display = "block";
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+        // modal.style.display = "block";
+        // backdrop.style.display = "block";
     });
 }
 
 backdrop.addEventListener("click", function() {
-    mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if(modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
+
 
 function closeModal() {
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+
+    if(modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function() {
